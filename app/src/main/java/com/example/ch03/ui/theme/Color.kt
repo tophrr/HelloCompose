94d2e0b6a6200c4abfd94530e0ba297d42f62226
Christopher G <59037609+tophrr@users.noreply.github.com>
2026-09-09 23:20:17 +0700

    feat: add theme color palette

--- app/src/main/java/com/example/ch03/ui/theme/Color.kt
+++ app/src/main/java/com/example/ch03/ui/theme/Color.kt
@@ -2,7 +2,6 @@ package com.example.ch03.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// TODO: Pelajari pasangan warna light dan dark, lalu ganti nilai sesuai README.
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
