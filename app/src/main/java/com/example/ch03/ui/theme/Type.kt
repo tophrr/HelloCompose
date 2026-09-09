37c7252d4b496223235fc5728f2fbf019eea2a07
Christopher G <59037609+tophrr@users.noreply.github.com>
2026-09-09 23:21:02 +0700

    feat: add base typography

--- app/src/main/java/com/example/ch03/ui/theme/Type.kt
+++ app/src/main/java/com/example/ch03/ui/theme/Type.kt
@@ -6,7 +6,6 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// TODO: Sesuaikan typography bila diperlukan untuk desain aplikasi Anda.
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
